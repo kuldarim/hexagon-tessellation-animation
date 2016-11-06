2ae8db11f6331d1f54795c5ea16a3b2c495d2aa5
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2016-11-06 11:03:09 -0800

    added logic to draw from needed line

--- hexagon-tessellation-animation.jsx
+++ hexagon-tessellation-animation.jsx
@@ -4,80 +4,83 @@ var proj= app.newProject();
 // constants
 var SIZE = 50;
 var TIME = 4;
+var COORDINATES = [{},{},{},{}];
 
 var myComp = proj.items.addComp("hexagon-tessellation",1280,720,1,TIME,25);
 myComp.openInViewer();
 
 drawHexagon("initial", 0, 0, 6, 0);
-drawHexagonRing(0,0,6,1);
-drawHexagonRing(0,0,6,2);
-drawHexagonRing(0,0,6,3);
+drawHexagonRing(0, 0, 6, 1);
+drawHexagonRing(0, 0, 6, 2);
+drawHexagonRing(0, 0, 6, 3);
 
 // http://www.redblobgames.com/grids/hexagons/ 
 // http://stackoverflow.com/questions/14916941/draw-a-hexagon-tessellation-animation-in-python
 function drawHexagonRing(x, y, n, ringsNumber) {
-    var dc = SIZE * Math.sqrt(3);
-    var xc = x;
-    var yc = y-ringsNumber*dc;
-    var dx = -dc* Math.sqrt(3)/2;
-    var dy = dc/2;
-    for (var i = 0; i < 6; i++) {
-        for (var j = 0; j < ringsNumber; j++) {
-            var xc = xc + dx;
-            var yc = yc + dy;
-            drawHexagon(i+'_'+j, xc, yc, n, ringsNumber);
-        }
-        var tmpdx = dx;
-        var tmpdy = dy;
-        dx = Math.cos(Math.PI/3)*tmpdx+Math.sin(Math.PI/3)*tmpdy;
-        dy = -Math.sin(Math.PI/3)*tmpdx+Math.cos(Math.PI/3)*tmpdy;
+  var dc = SIZE * Math.sqrt(3);
+  var xc = x;
+  var yc = y - ringsNumber * dc;
+  var dx = -dc * Math.sqrt(3) / 2;
+  var dy = dc / 2;
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < ringsNumber; j++) {
+      var xc = xc + dx;
+      var yc = yc + dy;
+      drawHexagon(i + '_' + j, xc, yc, n, ringsNumber);
     }
+    var tmpdx = dx;
+    var tmpdy = dy;
+    dx = Math.cos(Math.PI / 3) * tmpdx + Math.sin(Math.PI / 3) * tmpdy;
+    dy = -Math.sin(Math.PI / 3) * tmpdx + Math.cos(Math.PI / 3) * tmpdy;
+  }
 }
 
 function drawHexagon(name, x, y, n, ringsNumber) {
-    var shapeVertices = getPolyginVertices(n, SIZE, x, y);
-    addShape(name, shapeVertices, ringsNumber);
+  var shapeVertices = getPolyginVertices(n, SIZE, x, y, ringsNumber);
+  if (ringsNumber > 0) {
+    var index = getIndexFromCoordinates(shapeVertices, ringsNumber);
+    var modifiedVertices = changeOrder(shapeVertices, index);
+    addShape(name, modifiedVertices, ringsNumber);
+  } else {
+      addShape(name, shapeVertices, ringsNumber);
+  }
+  
 }
 
-function addShape(name, vertices, ringsNumber) {
-    var myShape = myComp.layers.addShape();
-    myShape.name = (name);
-    var myShapeContent1 = myShape.property("Contents").addProperty("ADBE Vector Group");
-    var myShapeGroup1 = myShape.property("Contents").property("Group 1");
+function changeOrder(array, index) {
+  var a1 = array.slice(0, index);
+  var a2 = array.slice(index);
+  return [].concat(a2).concat(a1);
+}
 
-    var myShapePath= myShapeGroup1.property("Contents").addProperty("ADBE Vector Shape - Group");
-    var myShapeMask = myShapePath.property("Path");
-    var myShapeM = myShapeMask.value;
-    myShapeM.vertices = vertices;
-    myShapeMask.setValue(myShapeM);
+function getIndexFromCoordinates(array, ringsNumber) {
+  var index = undefined;
+  for (var i = 0; i < array.length; i++) {
+    var element = array[i];
+    var x = element[0];
+    var y = element[1];
+    if (COORDINATES[ringsNumber - 1]['' + parseInt(x) + parseInt(y)]) {
+      index = i + 1;
+    }
+  }
 
-    myShapeGroup1.property("Contents")
-        .addProperty("ADBE Vector Graphic - Stroke")
-        .property("Color").setValue([0, 0, 0]);
-
-    var trim = myShape.property("Contents")
-        .addProperty("ADBE Vector Filter - Trim");
-
-    // add animation keyframes
-
-    trim.property("End").setValueAtTime(ringsNumber, 0);
-    trim.property("End").setValueAtTime(ringsNumber + 1, 100);
-
+  return index;
 }
 
 
 // http://www.storminthecastle.com/2013/07/24/how-you-can-draw-regular-polygons-with-the-html5-canvas-api/
 // http://scienceprimer.com/drawing-regular-polygons-javascript-canvas
-function getPolyginVertices(numberOfSides, size, Xcenter, Ycenter) {
-    var verticesArray = [];
- 
-    for (var i = 0; i <= numberOfSides; i++) {
-        var angleDeg = 60 * i;
-        var angleRad = Math.PI / 180 * angleDeg;
-        var x = Xcenter + size * Math.cos(angleRad);
-        var y = Ycenter + size * Math.sin(angleRad);
-        verticesArray.push([x,y]);
-    }
+function getPolyginVertices(numberOfSides, size, Xcenter, Ycenter, ringsNumber) {
+  var verticesArray = [];
 
-    return verticesArray;
+  for (var i = 0; i <= numberOfSides; i++) {
+    var angleDeg = 60 * i;
+    var angleRad = Math.PI / 180 * angleDeg;
+    var x = Xcenter + size * Math.cos(angleRad);
+    var y = Ycenter + size * Math.sin(angleRad);
+    verticesArray.push([x, y]);
+    COORDINATES[ringsNumber]['' + parseInt(x) + parseInt(y)] = true;
+  }
+
+  return verticesArray;
 }
